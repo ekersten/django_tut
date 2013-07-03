e887e7f403a091097909d058791710391f8ac4a7
Eric Kersten <ekersten@gmail.com>
2013-07-03 17:55:06 -0300

    progressed in the tutorial

--- django_tut/settings.py
+++ django_tut/settings.py
@@ -111,7 +111,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    'c:/wamp/www/django_tut/templates'
+    #'c:/wamp/www/django_tut/templates'
 )
 
 INSTALLED_APPS = (
--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,45 @@
-from django.http import HttpResponse
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import RequestContext, loader
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from django.utils import timezone
 
-def index(request):
-	return HttpResponse("Hello, world. You're at the poll index.")
+from polls.models import Choice, Poll
 
-def detail(request, poll_id):
-	return HttpResponse("You're looking at poll %s." % poll_id)
 
-def results(request, poll_id):
-	return HttpResponse("You're looking at the results of poll %s." % poll_id)
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_poll_list'
 
-def vote(request, poll_id):
-	return HttpResponse("You're voting on poll %s." % poll_id)
+	def get_queryset(self):
+		"""Return the last five published polls."""
+		return Poll.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+	model = Poll
+	template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+	model = Poll
+	template_name = 'polls/results.html'
+
+
+def vote(request, pk):
+	p = get_object_or_404(Poll, pk=pk)
+	try:
+		selected_choice = p.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		# Redisplay the poll voting form
+		return render(request, 'polls/defailt.html', {
+			'poll':p,
+			'error_message': "You didn't select a choice",
+			})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		# Always return an HttpResponseRedirect after successfully dealing
+		# with POST data. This prevents data from being posted twice if a
+		# user hits the Back button.
+		return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
